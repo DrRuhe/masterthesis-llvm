b8d4ce085eab57d710e1609202ba37cfbd454ed9
Jakob Gerhardt <jak.gerhardt@gmail.com>
2026-02-07 17:50:51 +0100

    try to pass correct flags

--- runtime-specialization/test/lit.cfg.py
+++ runtime-specialization/test/lit.cfg.py
@@ -18,25 +18,26 @@ llvm_config = lit.llvm.llvm_config
 # Tools aus dem Build-Tree bevorzugen.
 llvm_config.with_environment("PATH", config.llvm_tools_dir, append_path=True)
 
-# Standard-Substitutions
-llvm_shlib_dir = os.path.normpath(config.llvm_shlib_dir)
-config.substitutions.append(("%llvmshlibdir", llvm_shlib_dir))
-config.substitutions.append(("%shlibext", config.llvm_plugin_ext))
+llvm_config.use_clang(
+    additional_flags=[config.additional_clang_flags],
+)
 
-# Clang/Clang++ aus dem Build-Tree auflösen (nicht System clang).
-config.substitutions.append((
-    "%clang",
-    llvm_config.use_llvm_tool("clang", search_paths=[config.llvm_tools_dir], required=True),
-))
-config.substitutions.append((
-    "%clangxx",
-    llvm_config.use_llvm_tool("clang++", search_paths=[config.llvm_tools_dir], required=True),
-))
-
-import os
-
-if os.environ.get("LIT_DEBUG", "0") == "1":
-    lit_config.note(f"test_source_root = {config.test_source_root}")
-    lit_config.note(f"test_exec_root   = {config.test_exec_root}")
-    lit_config.note(f"llvm_tools_dir    = {getattr(config, 'llvm_tools_dir', None)}")
-    lit_config.note(f"llvm_shlib_dir    = {getattr(config, 'llvm_shlib_dir', None)}")
+#
+# # Standard-Substitutions
+# llvm_shlib_dir = os.path.normpath(config.llvm_shlib_dir)
+# config.substitutions.append(("%llvmshlibdir", llvm_shlib_dir))
+# config.substitutions.append(("%shlibext", config.llvm_plugin_ext))
+#
+# # Clang/Clang++ aus dem Build-Tree auflösen (nicht System clang).
+# config.substitutions.append((
+#     "%clang",
+#     llvm_config.use_llvm_tool("clang", search_paths=[
+#         config.llvm_tools_dir
+#     ], required=True,additional_flags=""),
+# ))
+# config.substitutions.append((
+#     "%clangxx",
+#     llvm_config.use_llvm_tool("clang++", search_paths=[
+#         config.llvm_tools_dir
+#     ], required=True),
+# ))
