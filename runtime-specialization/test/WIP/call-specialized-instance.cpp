c752608083d007c644e7d833560d8fd6ca3c488e
Jakob Gerhardt <jak.gerhardt@gmail.com>
2026-02-25 17:28:52 +0100

    cleanup

--- runtime-specialization/test/WIP/call-specialized-instance.cpp
+++ runtime-specialization/test/WIP/call-specialized-instance.cpp
@@ -1,5 +1,4 @@
 // RUN: %clangxx -g -O0 -emit-llvm -c %s -o %t.bc
-// RUN: opt -S %t.bc --strip-debug -o -
 // RUN: opt --verify-debuginfo-preserve -load-pass-plugin=%llvmshlibdir/LLVMRuntimeSpecializationComptimePlugin%shlibext -passes='runtime-specialization-IR-dumping' %t.bc -o %t.opt.bc
 // RUN: %clangxx -g %t.opt.bc -o %t.exe
 // RUN: %t.exe 1
@@ -21,15 +20,12 @@ public:
 
   int getMod2() const __asm__("A::getMod2") {
     int result = value % 2;
-    std::fprintf(stderr, "[getMod2] return value: %d\n", result);
     return result;
   }
 
   int add(int a, int b) const __asm__("A::add")
   {
-    std::fprintf(stderr, "[add] args: a=%d, b=%d, value=%d\n", a, b, value);
     int result = value + a + b;
-    std::fprintf(stderr, "[add] return value: %d\n", result);
     return result;
   }
 };
@@ -43,7 +39,6 @@ int main(int argc, char** argv) {
   {
     throw std::runtime_error("[ClangRuntimeSpecializer] could not init! ");
   }
-  // TODO Could the reason why getMod2 is not found be that its removed by dead code analysis? YES
   // We can add a FileCheck
   int r1 = RuntimeSpecializer->template call_specialized<int>("A::getMod2", instance);
   int r2 = RuntimeSpecializer->template call_specialized<int>("A::add", instance, 7, 11);
