8c8d9797a9ed23a65186bbef17d28455ab90e519
Jakob Gerhardt <jak.gerhardt@gmail.com>
2026-02-27 08:24:09 +0100

    adjust tests

--- runtime-specialization/test/WIP/pure-specialized-method-is-equivalent.cpp
+++ runtime-specialization/test/WIP/pure-specialized-method-is-equivalent.cpp
@@ -1,5 +1,4 @@
 // RUN: %clangxx -g -O0 -emit-llvm -c %s -o %t.bc
-// RUN: opt --strip-debug -S %t.bc|grep addAndSum
 // RUN: opt --verify-debuginfo-preserve -load-pass-plugin=%llvmshlibdir/LLVMRuntimeSpecializationComptimePlugin%shlibext -passes='runtime-specialization-IR-dumping' %t.bc -o %t.opt.bc
 // RUN: %clangxx -g %t.opt.bc -o %t.exe
 // RUN: %t.exe 1
@@ -10,6 +9,8 @@
 #include "ClangRuntimeSpecializer.h"
 #include <cstdio>
 
+// PRE-DUMP-NOT: RuntimeSpecializeableIR_ptr
+// POST-DUMP: RuntimeSpecializeableIR_ptr
 
 class A {
   int value;
@@ -24,21 +25,26 @@ public:
     return !(*this == other);
   }
 
-
-  int addAndSum(int x) __asm__("A::addAndSum") {
-	std::fprintf(stderr, "[addAndSum] was called!\n");
-	value += x;
-    return value;
+  int getMod2() const __asm__("A::getMod2") {
+    int result = value % 2;
+    return result;
   }
 
+  int add(int a, int b) const __asm__("A::add")
+  {
+    int result = value + a + b;
+    return result;
+  }
 };
 
-inline constexpr char Fn_A_addAndSum[] = "A::addAndSum";
+inline constexpr char Fn_A_getMod2[] = "A::getMod2";
+inline constexpr char Fn_A_add[] = "A::add";
 
 int main(int argc, char** argv) {
 
-  A instance(12);
+  A instance(argc);
 
-  clangRuntimeSpecializer::assertSpecializedMethodIsEquivalent<Fn_A_addAndSum>(&A::addAndSum, instance,argc);
+  clangRuntimeSpecializer::assertSpecializedMethodIsEquivalent<Fn_A_getMod2>(&A::getMod2, instance);
+  clangRuntimeSpecializer::assertSpecializedMethodIsEquivalent<Fn_A_add>(&A::add, instance,7, 11);
   return 0;
 }
